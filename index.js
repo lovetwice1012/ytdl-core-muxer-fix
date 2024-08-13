2c2278707af069e8b32435fb4bb5706c300d2c1b
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2024-08-13 22:06:59 +0900

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const stream = require('stream');
 const ytmux = (link, agent, options = {}) => {
     const result = new stream.PassThrough({ highWaterMark: options.highWaterMark || 1024 * 512 });
     ytdl.getInfo(link, options).then(info => {
-        audioStream = ytdl.downloadFromInfo(info, { ...options, quality: 'highestaudio', agent: agent });
-        videoStream = ytdl.downloadFromInfo(info, { ...options, quality: 'highestvideo', agent: agent });
+        audioStream = ytdl(info, {quality: 'highestaudio', agent: agent });
+        videoStream = ytdl(info, {quality: 'highestvideo', agent: agent });
         // create the ffmpeg process for muxing
         ffmpegProcess = cp.spawn(ffmpegPath, [
             // supress non-crucial messages
